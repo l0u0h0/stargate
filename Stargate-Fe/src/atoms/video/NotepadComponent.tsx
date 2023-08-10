6f87bcf081a54154ed798d498832e3094d85afd6
l0u0h0 <l0u0h0@gmail.com>
2023-08-10 10:55:46 +0900

    Feature: Drag n Drop Add

--- Stargate-Fe/src/atoms/video/NotepadComponent.tsx
+++ Stargate-Fe/src/atoms/video/NotepadComponent.tsx
@@ -4,43 +4,45 @@ const NotepadComponent = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const dragComponentRef = useRef<HTMLDivElement>(null);
   const [origin, setOrigin] = useState({ x: 0, y: 0 });
-  const [client, setClient] = useState({ x: 0, y: 0 });
+  const [current, setCurrent] = useState({ x: 0, y: 0 });
   const [pos, setPos] = useState({ left: 0, top: 0 });
-  
+
   const isInsideDragArea = (e: React.DragEvent<HTMLElement>) => {
-    const left = (e.target instanceof HTMLElement) ? e.target.offsetLeft : 0;
-    const top = (e.target instanceof HTMLElement) ? e.target.offsetTop : 0;
-    const bottom = (e.target instanceof HTMLElement) ? top + e.target.offsetHeight : 0;
-    const right = (e.target instanceof HTMLElement) ? left + e.target.offsetWidth : 0;
+    const left = e.target instanceof HTMLElement ? e.target.offsetLeft : 0;
+    const top = e.target instanceof HTMLElement ? e.target.offsetTop : 0;
+    const bottom =
+      e.target instanceof HTMLElement ? top + e.target.offsetHeight : top;
+    const right =
+      e.target instanceof HTMLElement ? left + e.target.offsetWidth : left;
 
-    console.log(containerRef.current?.offsetWidth)
+    const isValid = (num: number) => {
+      if (num >= 300 || num <= 0) return false;
+      return true;
+    }
 
-    console.log(left, top, bottom, right);
-
-    const containerLeft = containerRef.current?.offsetLeft != undefined ? containerRef.current?.offsetLeft : 0;
-    const containerTop = containerRef.current?.offsetTop != undefined ? containerRef.current?.offsetTop : 0;
-    const containerBottom = containerRef.current?.offsetHeight != undefined ? containerTop - containerRef.current?.offsetHeight : containerTop + 0;
-    const containerRight = containerRef.current?.offsetWidth != undefined ? containerLeft + containerRef.current?.offsetWidth : containerLeft + 0;
-    
-    console.log({ containerLeft, containerTop, containerBottom, containerRight })
-    
-    if (left < containerLeft || top > containerTop || bottom < containerBottom || right > containerRight) return false;
-    return true;
-  }
+    if (isValid(left) && isValid(top) && isValid(bottom) && isValid(right)) return true;
+    return false;
+  };
 
   const dragHandler = (e: React.DragEvent<HTMLElement>) => {
     const posTemp = { ...pos };
-    posTemp['left'] = (e.target instanceof HTMLElement) ? e.target.offsetLeft + e.clientX - client.x : 0;
-    posTemp['top'] = (e.target instanceof HTMLElement) ? e.target.offsetTop + e.clientY - client.y : 0;
+    posTemp['left'] =
+      e.target instanceof HTMLElement
+        ? e.target.offsetLeft + e.clientX - current.x
+        : origin.x;
+    posTemp['top'] =
+      e.target instanceof HTMLElement
+        ? e.target.offsetTop + e.clientY - current.y
+        : origin.y;
 
     setPos(posTemp);
 
-    const clientPosTemp = { ...client };
-    clientPosTemp['x'] = e.clientX;
-    clientPosTemp['y'] = e.clientY;
+    const currentPosTemp = { ...current };
+    currentPosTemp['x'] = e.clientX;
+    currentPosTemp['y'] = e.clientY;
 
-    setClient(clientPosTemp);
-  }
+    setCurrent(currentPosTemp);
+  };
 
   const dragStartHandler = (e: React.DragEvent<HTMLDivElement>) => {
     const blankCanvas = document.createElement('canvas');
@@ -49,30 +51,28 @@ const NotepadComponent = () => {
     document.body.appendChild(blankCanvas);
     e.dataTransfer.effectAllowed = 'move';
     const originPosTemp = { ...origin };
-    originPosTemp['x'] = (e.target instanceof HTMLElement) ? e.target.offsetLeft : 0;
-    originPosTemp['y'] = (e.target instanceof HTMLElement) ? e.target.offsetTop : 0;
-    console.log('originPosTemp: ', originPosTemp);
+    originPosTemp['x'] =
+      e.target instanceof HTMLElement ? e.target.offsetLeft : origin.x;
+    originPosTemp['y'] =
+      e.target instanceof HTMLElement ? e.target.offsetTop : origin.y;
     setOrigin(originPosTemp);
 
-    const clientPosTemp = { ...client };
-    clientPosTemp['x'] = e.clientX;
-    clientPosTemp['y'] = e.clientY;
-    setClient(clientPosTemp);
-    console.log('clientPosTemp: ', clientPosTemp);
-  }
+    const currentPosTemp = { ...current };
+    currentPosTemp['x'] = e.clientX;
+    currentPosTemp['y'] = e.clientY;
+    setCurrent(currentPosTemp);
+  };
 
   const dragOverHandler = (e: React.DragEvent<HTMLElement>) => {
     e.preventDefault();
-  }
+  };
 
   const dragEndHandler = (e: React.DragEvent<HTMLElement>) => {
-    if (isInsideDragArea(e)) {
+    if (!isInsideDragArea(e)) {
       const posTemp = { ...pos };
       posTemp['left'] = origin.x;
       posTemp['top'] = origin.y;
       setPos(posTemp);
-    } else {
-      console.log(pos);
     }
 
     const canvases = document.getElementsByClassName('canvas');
@@ -81,23 +81,23 @@ const NotepadComponent = () => {
       canvas.parentNode?.removeChild(canvas);
     }
     document.body.removeAttribute('style');
-  }
+  };
 
   return (
-    <div className="container w-screen h-screen z-50" ref={containerRef}>
-      <div
-        className="drag-component w-10 h-10 bg-slate-400"
-        ref={dragComponentRef}
-        draggable
-        onDrag={(e) => dragHandler(e)}
-        onDragStart={(e) => dragStartHandler(e)}
-        onDragOver={(e) => dragOverHandler(e)}
-        onDragEnd={(e) => dragEndHandler(e)}
-        style={{ left: pos.left, top: pos.top }}
-      >
-        {/* <textarea className="bg-yellow-200 resize">hi</textarea> */}
+      <div className="absolute container w-300 h-300 z-50" ref={containerRef}>
+        <div
+          className="relative drag-component w-10 h-10 bg-slate-400"
+          ref={dragComponentRef}
+          draggable
+          onDrag={(e) => dragHandler(e)}
+          onDragStart={(e) => dragStartHandler(e)}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDragEnd={(e) => dragEndHandler(e)}
+          style={{ left: pos.left, top: pos.top }}
+        >
+          <textarea className="bg-yellow-200 resize">hi</textarea>
+        </div>
       </div>
-    </div>
   );
 };
 
--- Stargate-Fe/src/organisms/video/VideoHeaderComponent.tsx
+++ Stargate-Fe/src/organisms/video/VideoHeaderComponent.tsx
@@ -1,14 +1,18 @@
+import TimeLeftComponent from '@/atoms/common/TimeLeftComponent';
 import ProFileIcon from '@/atoms/video/ProFileIcon';
-import React from 'react';
+import React, { useState } from 'react';
 
 const VideoHeaderComponent = () => {
+  const [time, setTime] = useState({ min: 0, sec: 0 });
+
   return (
     <div className="flex grid-col-4">
       <div className="col-span-1">
         <ProFileIcon />
       </div>
       <div className="col-span-3">
-        
+        <TimeLeftComponent min={time.min} sec={time.sec} />
+
       </div>
     </div>
   );
